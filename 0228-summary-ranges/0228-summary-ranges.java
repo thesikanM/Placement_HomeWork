85cb5179a45002169286a405fd5bf5cc80fce29f
Thesikan M <thesigan.gpm@gmail.com>
2026-07-02 10:31:39 +0530

    Time: 3 ms (78.78%), Space: 43 MB (53.57%) - LeetHub

---
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String>list =new ArrayList<>();
+      int i=0;
+      while(i<nums.length)
+        {
+            int start =nums[i];
+            while(i<nums.length-1 && nums[i]+1 == nums[i+1])
+            {
+                i++;
+            }
+           if(start ==nums[i])
+           {
+            list.add(String.valueOf(start));
+           }       
+           else
+           {
+           list.add(start+"->"+nums[i]);
+           }
+           i++;
+        }
+        return list;
+    }
+}
