11663d681efef1c3714e4bca35eb48c588b034a7
Thesikan M <thesigan.gpm@gmail.com>
2026-07-02 10:57:53 +0530

    Time: 3 ms (78.78%), Space: 43.3 MB (20.14%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.java
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -3,22 +3,22 @@ class Solution {
         List<String>list =new ArrayList<>();
       int i=0;
       while(i<nums.length)
+      {
+        int start =nums[i];
+        while(i<nums.length-1 && nums[i]+1==nums[i+1])
         {
-            int start =nums[i];
-            while(i<nums.length-1 && nums[i]+1 == nums[i+1])
-            {
-                i++;
-            }
-           if(start ==nums[i])
-           {
-            list.add(String.valueOf(start));
-           }       
-           else
-           {
-           list.add(start+"->"+nums[i]);
-           }
-           i++;
+            i++;
         }
-        return list;
+        if(start ==nums[i])
+        {
+            list.add(String.valueOf(start));
+        }
+        else
+        {
+            list.add(start+"->"+nums[i]);
+        }
+        i++;
+      }
+      return list;
     }
 }
