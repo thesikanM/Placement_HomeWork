d66b7fc76a57077e46f8df7931155fbb77703651
Thesikan M <thesigan.gpm@gmail.com>
2026-07-31 23:29:27 +0530

    Time: 0 ms (100%), Space: 43.2 MB (47.1%) - LeetHub

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minimumPushes(String word) {
+        int ans =0;
+        for(int i=0;i<word.length();i++)
+        {
+            if(i<8)
+            {
+                ans+=1;
+            }
+            else if(i<16)
+            {
+                ans+=2;
+            }
+            else  if(i<24)
+            {
+                ans+=3;
+            }
+            else
+            {
+                ans+=4;
+            }
+        }
+        return ans;
+    }
+}
