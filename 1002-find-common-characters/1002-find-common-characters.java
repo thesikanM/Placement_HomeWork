2580ce5372a1e914d8f786c29a4137493bd1be6a
Thesikan M <thesigan.gpm@gmail.com>
2026-08-03 23:41:29 +0530

    Time: 9 ms (21.16%), Space: 47.2 MB (7.29%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.java
+++ 1002-find-common-characters/1002-find-common-characters.java
@@ -1,31 +1,3 @@
-/*class Solution {
-    public List<String> commonChars(String[] words) {
-         ArrayList<String>list =new ArrayList<>();
-         String first =words[0];
-         for(char ch:first.toCharArray())
-         {
-            boolean commen =true;
-            for(int i=1;i<words.length;i++)
-            {
-                int ind =words[i].indexOf(ch);
-
-                if(ind == -1)
-                {
-                    commen =false;
-                    break;
-                }
-                //words[i]=words[i].substring(1,ind)+words[i].substring(ind+1);
-                words[i]=words[i].substring(1, ind) + words[i].substring(ind + 1);
-            }
-            if(commen)
-            {
-                list.add(String.valueOf(ch));
-            }
-           
-         }
-          return list;
-    }
-}*/
 class Solution {
     public List<String> commonChars(String[] words) {
 
